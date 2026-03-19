9b227902b7817adc6fbc3e224c3192c371f0efda
WintanaGebrehiwot <119295945+WintanaGebrehiwot@users.noreply.github.com>
2026-03-19 11:12:01 +0300

    Add solution for String Compression (trial 1)

---
+++ leetcode/string-compression_trial1.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def compress(self, chars):
+        """
+        :type chars: List[str]
+        :rtype: int
+        """
+        read = 0
+        write = 0
+
+        while read < len(chars):
+            current_char = chars[read]
+            count = 0
+
+            while read < len(chars) and chars[read] == current_char:
+                read += 1
+                count += 1
+
+            chars[write] = current_char
+            write += 1
+
+            if count > 1:
+                for digit in str(count):
+                    chars[write] = digit
+                    write += 1
+
+        return write
+        
+
+        
